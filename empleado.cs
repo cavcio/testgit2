684046a3c582ef0c376f7c99974102d65c3ada86
Carlos A Vasquez Aviles <cvasquez_Aviles@hotmail.com>
2025-07-19 06:55:09 -0600

    3er commit

---
+++ empleado.cs
@@ -0,0 +1,8 @@
+class empleado
+{
+    
+    public int id { get; set; }
+    public string PrimerNombre { get; set; }
+    public string SegundoNombre { get; set; }
+    public decimal Salario { get; set; }
+}
---
+++ user.cs
@@ -0,0 +1,8 @@
+class user
+{
+    public int id { get; set; }
+    public string PrimerNombre { get; set; }
+    public string SegundoNombre { get; set; }
+    
+    
+}
